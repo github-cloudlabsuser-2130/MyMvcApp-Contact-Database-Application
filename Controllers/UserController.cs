6e9b4fd4e5d6f39e6db1c288c48689ac00e3dcbe
John Doe <john.doe@example.com>
2024-10-16 09:39:09 +0000

    Update cache files for MyMvcApp

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,25 +12,54 @@ public class UserController : Controller
         public ActionResult Index()
         {
             // Implement the Index method here
+                        // GET: User
+
+            // Retrieve the list of users
+            var users = userlist;
+            
+            // Pass the list to the view
+            return View(users);
+
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            // Find the user by ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            
+            // If user is not found, return NotFound
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
+            // Pass the user to the view
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            // Return the Create view
+            return View();
         }
 
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                // Add the user to the list
+                userlist.Add(user);
+                
+                // Redirect to the Index action
+                return RedirectToAction(nameof(Index));
+            }
+            
+            // If model state is not valid, return the Create view with the user model
+            return View(user);
         }
 
         // GET: User/Edit/5
@@ -38,6 +67,14 @@ public class UserController : Controller
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
+            return View(user);
         }
 
         // POST: User/Edit/5
@@ -49,18 +86,48 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            if (ModelState.IsValid)
+            {
+                var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                if (existingUser == null)
+                {
+                    return NotFound();
+                }
+
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
         }
 
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, IFormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction(nameof(Index));
         }
 }
